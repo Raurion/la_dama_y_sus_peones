3a731c60ace37234ae378a3ad9e12b1d26a64a63
Cobos Melgares external <sancobosm@gmail.com>
2023-03-29 18:23:47 +0200

    PRIMER MOVIMIENTO ALEATORIO SUUUUIIIII

--- src/main/java/los/internos/Main.java
+++ src/main/java/los/internos/Main.java
@@ -1,7 +1,22 @@
 package los.internos;
 
+import los.internos.api.Controller;
+
+import java.util.AbstractSet;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Controller controller = new Controller();
+        System.out.println(controller.getPositions());
+        controller.setWhites();
+        System.out.println(controller.getWhites());
+        System.out.println(controller.getAvailableMoves());
+        Scanner sc = new Scanner(System.in);
+        System.out.println(controller.getFrom());
+        System.out.println(controller.getTo());
+        sc.nextLine();
+        controller.moveToFrom();
+
     }
 }
--- src/main/java/los/internos/api/Controller.java
+++ src/main/java/los/internos/api/Controller.java
@@ -3,18 +3,26 @@ package los.internos.api;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 public class Controller {
     private String token;
     private String uuid;
+    private List<String> whites;
+    private HashMap<String, Integer> positions;
+    private String from;
+    private String to;
 
     public Controller() {
         Unirest.setTimeouts(0, 0);
-        try{
+        try {
             //token
             HttpResponse<JsonNode> responseToken = Unirest.post("http://localhost:8080/oauth/token?username=admin&password=admin&grant_type=password")
                     .header("Authorization", "Basic Y2xpZW50SWQ6c2VjcmV0")
@@ -26,7 +34,7 @@ public class Controller {
 
             //uuid
             HttpResponse<JsonNode> responseUuid = Unirest.post("http://localhost:8080/api/v1/game/create")
-                    .header("Authorization", "Bearer "+token)
+                    .header("Authorization", "Bearer " + token)
                     .header("Content-Type", "application/x-www-form-urlencoded")
                     .field("side", "WHITE")
                     .field("againstComputer", "false")
@@ -37,13 +45,18 @@ public class Controller {
             System.out.println("Match UUID:");
             System.out.println(uuid);
 
-        }catch(Exception e){
+            positions = new HashMap<>();
+            setGamePositions();
+
+            whites = new ArrayList<>();
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void getGamePositions(){
-        try{
+    public void setGamePositions() {
+        try {
             HttpResponse<String> responseGetGamePositions = Unirest.get("http://localhost:8080/api/v1/game/pieces?uuid=" + uuid
                     )
                     .header("Authorization", "Bearer " + token)
@@ -52,17 +65,119 @@ public class Controller {
 
             String responseBody = responseGetGamePositions.getBody();
             JSONArray jsonArray = new JSONArray(responseBody);
-            HashMap<String, Integer> positions = new HashMap<>();
 
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 String rawPosition = jsonObject.getString("rawPosition");
                 Integer sides = jsonObject.getInt("side");
-                positions.put(rawPosition, sides);
+                this.positions.put(rawPosition, sides);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
+    public void setWhites() {
+        this.positions.forEach((pieza, raza) -> {
+            if (raza == 1) {
+                whites.add(pieza);
+                System.out.println("key: " + pieza + " value:" + raza);
+            }
+        });
+    }
+
+    public List<String> getWhites() {
+        return whites;
+    }
+
+    public HashMap<String, Integer> getPositions() {
+        return positions;
+    }
+
+    /*public boolean getIfAvailableMoves(String from){
+        try {
+            HttpResponse<String> responseGetAvailableMoves = Unirest.get("http://localhost:8080/api/v1/game/available-moves?from="+ from +"&uuid="+this.uuid)
+                    .header("Authorization", "Bearer "+this.token)
+                    .asString();
+            String resultado = responseGetAvailableMoves.getBody();
+            if(resultado.equals("[]")){
+                return true;
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }*/
+
+    public String getAvailableMoves() {
+        try {
+            ArrayList<String> whitesCanMove = new ArrayList<>();
+            for (int i = 0; i < whites.size(); i++) {
+                HttpResponse<String> responseGetAvailableMoves = Unirest.get("http://localhost:8080/api/v1/game/available-moves?from=" + whites.get(i) + "&uuid=" + this.uuid)
+                        .header("Authorization", "Bearer " + this.token)
+                        .asString();
+                String resultado = responseGetAvailableMoves.getBody();
+                if (!resultado.equals("[]")) {
+                    whitesCanMove.add(whites.get(i));
+                }
+            }
+
+            //ramdon para elegir una blanca
+            int random = (int) (Math.random() * (whites.size() - 0));
+            HttpResponse<String> responseGetAvailableMoves = Unirest.get("http://localhost:8080/api/v1/game/available-moves?from=" + whites.get(random) + "&uuid=" + this.uuid)
+                    .header("Authorization", "Bearer " + this.token)
+                    .asString();
+            String resultado = responseGetAvailableMoves.getBody();
+            //aqui pongo el from que se va a usar
+            from = whites.get(random);
+
+            //aqui voy a coger las posiciones
+            String responseBody = responseGetAvailableMoves.getBody();
+            JSONArray jsonArray = new JSONArray(responseBody);
+            //random = (int)(Math.random()*(jsonArray.length()-0));
+            // System.out.println(random);
+
+            //  int i = random;
+            String[] array = new String[jsonArray.length()];
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                int row = jsonObject.getInt("row");
+                String col = jsonObject.getString("col");
+                array[i] = col.toUpperCase() + row;
+            }
+
+            random = (int) (Math.random() * (array.length - 0));
+            System.out.println(random);
+
+            to = array[random];
+
+            return whitesCanMove.toString();
+        } catch (UnirestException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void getMovablePieces() {
+
+    }
+
+    public void moveToFrom() {
+        try {
+            HttpResponse<String> responseMoveTo = Unirest.post("http://localhost:8080/api/v1/game/move?uuid=" + uuid
+                            + "&from=" + this.from + "&to=" + this.to)
+                    .header("Authorization", "Bearer " + token)
+                    .asString();
+
+        } catch (Exception e) {
+        }
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
 }
